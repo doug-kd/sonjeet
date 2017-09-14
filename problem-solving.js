7e56243f0a12def814d3b81ae7bbb9f387a7a809
doug-kd <31511553+doug-kd@users.noreply.github.com>
2017-09-14 08:14:40 +0100

    Answers

--- problem-solving.js
+++ problem-solving.js
@@ -2,3 +2,63 @@ function getQuestionOne() {
 	let answer = document.getElementById("answer-1");
 	answer.innerHTML = "document.getElementById(elementName);";
 }
+
+function getQuestionTWO(){
+let answer = document.getElementById("answer-2");
+answer.innerHTML = "document.createElement('H1')"
+	let answer
+}
+
+function getQuestionTHREE(){
+let answer = document.getElementById("answer-3");
+answer.innerHTML = "element.setAttribute('Class','Name')";
+	let answer
+}
+
+function getQuestionFOUR(){
+let answer = document.getElementById("answer-4");
+answer.innerHTML = "element.innerHTML=Your Text";
+	let answer
+}
+
+function getQuestionFIVE(){
+let answer = document.getElementById("answer-5");
+answer.innerHTML = "document.createElement('p')";
+	let answer
+}
+
+function getQuestionSIX(){
+let answer = document.getElementById("answer-6");
+answer.innerHTML = "1) Identify the parent -
+
+document.getElementById('element Id') and save in a var
+eg var PARENT = document.getElementById('element Id')
+ 										2) Remove Child -
+
+Exmaple Parent.removechild(Parent.childNodes[0]);"
+	let answer
+}
+
+function getQuestionSEVEN(){
+	let answer = document.getElementById("answer-7");
+answer.innerHTML = "document.getElementById('Input_Id').value
+or document.getElementsByClassName('Search_Class')[0].value;"
+	let answer
+}
+
+function getQuestionEIGHT(){
+	let answer = document.getElementById("answer-8");
+answer.innerHTML = "document.getElementById('Input_Id').value
+or document.getElementsByClassName('Search_Class')[0].value;"
+	let answer
+}
+
+function getQuestionNINE(){
+	let answer = document.getElementById("answer-9");
+answer.innerHTML = "elementObject.style.display = "none";
+"
+	let answer
+}function getQuestionTEN(){
+	let answer = document.getElementById("answer-10");
+answer.innerHTML = "edit CSS file";
+"
